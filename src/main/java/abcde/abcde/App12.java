1b0c55ca86afe9a2eff75a18d3c2cd138da8605d
unknown <sudheerqa.selenium@gmail.com>
2021-01-28 15:08:20 -0600

    added User stories of DealsPage

---
+++ src/main/java/abcde/abcde/App12.java
@@ -0,0 +1,13 @@
+package abcde.abcde;
+
+/**
+ * Hello world!
+ *
+ */
+public class App12 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/abcde/abcde/App9.java
@@ -0,0 +1,13 @@
+package abcde.abcde;
+
+/**
+ * Hello world!
+ *
+ */
+public class App9 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );System.out.println( "Hello World!" );
+    }
+}
